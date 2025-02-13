29ab777daa3b79f37209483e8c1f3bb2d597e889
Suman Kumar Mishra <sumanmishra.tech@gmail.com>
2025-02-14 02:30:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('login-form');
+const codeInput = document.getElementById('code');
+const submitBtn = document.getElementById('submit-btn');
+const resultDiv = document.getElementById('result');
+
+const secretCode = 'Sweetheart'; 
+const nextPageUrl = 'main.html'; 
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const userInput = codeInput.value.trim();
+	if (userInput === secretCode) {
+		resultDiv.innerHTML = 'Baby';
+		window.location.href = nextPageUrl; // Redirect to new page
+	} else {
+		resultDiv.innerHTML = 'Na Tum meri chotuu nhi ho skti, jao meri shortcake ko lekr aao';
+	}
+});
